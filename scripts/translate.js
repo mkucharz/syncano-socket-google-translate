f56426f31513db90be9e4afffce8ee29e1e3716d
mkucharz <m@kucharz.net>
2017-09-01 08:32:00 +0200

    Adding language translations.

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,4 +1,5 @@
 import googleTranslate from 'google-translate'
+import langs from 'langs'
 import { response, event, socket } from 'syncano-server'
 
 const translator = googleTranslate(CONFIG.API_KEY);
@@ -6,7 +7,9 @@ const translator = googleTranslate(CONFIG.API_KEY);
 const text = ARGS.text
 const lang = ARGS.lang
 
-translator.translate(text, lang, function(err, translation) {
+const langCode = langs.where('name', lang.charAt(0).toUpperCase() + lang.slice(1))['1']
+
+translator.translate(text, langCode, function(err, translation) {
   if (err) {
     return response(text)
   }
